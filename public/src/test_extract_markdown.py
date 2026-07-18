ea41355485ed57103442816083bb6ea5f4dafad9
senras.boot <isenras@hotmail.com>
2026-07-18 13:14:59 +0000

    added extract func of images and links

---
+++ public/src/test_extract_markdown.py
@@ -0,0 +1,16 @@
+import unittest
+from extract_markdown import extract_markdown_images, extract_markdown_links
+
+class TestExtractMarkdown(unittest.TestCase):
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://example.com)"
+        )
+        self.assertListEqual([("link", "https://example.com")], matches)
